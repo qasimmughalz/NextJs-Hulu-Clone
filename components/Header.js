c0de98d375bac11e282e5bdcd7547618f6673723
Muhammad Qasim <imrqasim@gmail.com>
2022-10-22 10:54:36 -0700

    Setting up header and layout

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+            <h1>This is heading</h1>
+            <img src='https://png.pngtree.com/png-vector/20190303/ourmid/pngtree-modern-abstract-3d-logo-png-image_771012.jpg' className='object-contain' height={100} width={200}/>
+    </header>
+  )
+}
+
+export default Header
